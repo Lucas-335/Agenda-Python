a55dc5cd40907cbe031b4d79adebcc93924c96e5
Lucas Aguiar <lucas.335@live.com>
2024-04-02 21:35:59 -0300

    adicionando templates e static

---
+++ contacts/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path, include
+from contacts import views
+urlpatterns = [
+    path('', views.contacts)
+]
--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def contacts(requests):
+    return render(
+        requests,
+        'contacts/index.html'
+        )
